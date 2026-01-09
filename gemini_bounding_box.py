8ec9eaf03ec867ed30b5cf9ee8d257f017e2816b
Sachin <sachin@ahaar.in>
2026-01-09 18:18:20 +0530

    Final version

--- gemini_bounding_box.py
+++ gemini_bounding_box.py
@@ -31,48 +31,82 @@ def get_smart_crop_box(image_path):
         contents=[prompt, img]
     )
 
+    # text = response.text.replace("```json", "").replace("```", "")
+    # return json.loads(text)
     text = response.text.replace("```json", "").replace("```", "")
-    return json.loads(text)
+    parsed = json.loads(text)
+    print(f"DEBUG = Gemini returned: {parsed}")
+    print(f"DEBUG - Type: {type(parsed)}")
+    return parsed
 
-def parse_gemini_bbox(gemini_response):
-    """
-    Parse gemini response into a standard format
-    Convert to [xmin, xmax, ymin, ymax]
-    Args:
-        gemini_response: raw list from gemini
-    Returns:
-        dict with normalized coordinates in named format
-    """
 
-    detection = gemini_response[0]
-    box = detection['box_2d']
+# Step 3
+def generate_video_stream(cropped_image_path):
+    print("Uploading crop to Veo for latent conditioning...")
 
-    return {
-        'ymin': box[0],
-        'xmin': box[1],
-        'ymax': box[2],
-        'xmax': box[3],
-        'label': detection.get('label', 'unknown')
-    }
+    # Upload the file to Gemini API storage so Veo can access it
+    # file_upload = client.files.upload(file=cropped_image_path)
+    with open(cropped_image_path, "rb") as f:
+        image_bytes = f.read()
 
+    if cropped_image_path.endswith(".jpg") or cropped_image_path.endswith(".jpeg"):
+        mime_type="image/jpeg"
+    elif cropped_image_path.endswith(".png"):
+        mime_type="image/png"
+    else:
+        mime_type="image/jpeg"
+
+    image = types.Image(image_bytes=image_bytes, mime_type=mime_type)
+
+    prompt = "Cinematic, slow pan, luxury hotel atmosphere, 4k, highly detailed, soft sunlight."
+
+    print("Dreaming (Generating Video)...")
+
+    # Call Veo 3.1
+    # Note: 'image' parameter forces Image-to-Video mode
+    # print(types.GenerateVideosConfig.model_fields.keys())
+
+    operation = client.models.generate_videos(
+        model="veo-3.1-generate-preview",
+        prompt=prompt,
+        image=image,
+        config=types.GenerateVideosConfig(
+            aspect_ratio="9:16", # Redundant but good safety
+        )
+    )
+
+    # Poll for completion (Video generation takes time)
+    while not operation.done:
+        print("Veo is rendering...")
+        time.sleep(10)
+        operation = client.operations.get(operation)
+
+    # Retrieve Result
+    generated_video = operation.response.generated_videos[0]
+    return generated_video
 
 
 # --- MAIN EXECUTION PIPELINE ---
 def run_full_pipeline(source_image):
     # 1. Analyze
     print("Step 1: Gemini analyzing composition...")
-    bbox = get_smart_crop_box(source_image)
+    bbox_list = get_smart_crop_box(source_image)
+    bbox_dict = utils.parse_gemini_bbox(bbox_list)
 
     # 2. Crop
     print("Step 2: NB2 Pro cropping...")
-    cropped_file = (execute_precision_crop.execute_precision_crop(source_image, bbox))
+    cropped_file = execute_precision_crop.execute_precision_crop(source_image, bbox_dict)
 
     # 3. Generate
     print("Step 3: Veo generating video...")
-    video_result = generate_video_stream.generate_video_stream(cropped_file)
+    video_result = generate_video_stream(cropped_file)
 
     # 4. Save
-    print("Download complete.")
+    print("Downloading video")
+    client.files.download(file=video_result.video)
+    output_filename = "generated_video_9_16.mp4"
+    video_result.video.save(output_filename)
+    print(f"Download complete. Video saved to {output_filename}")
     # (Add download logic here based on 'video_result.video.uri')
 
 run_full_pipeline("hotel_room.jpg")
--- utils.py
+++ utils.py
@@ -19,3 +19,32 @@ def denormalize_bbox(bbox, image_width, image_height, scale=1000):
         'ymax': int((bbox['ymax'] / scale) * image_height),
     }
 
+def parse_gemini_bbox(gemini_response):
+    """
+    Parse gemini response into a standard format
+    Handle multiple response formats since we are using free form prompting
+    Convert to [xmin, xmax, ymin, ymax]
+    Args:
+        gemini_response: raw list from gemini
+    Returns:
+        dict with normalized coordinates in named format
+    """
+
+    if isinstance(gemini_response, dict):
+        if all(key in gemini_response for key in['ymin', 'xmin', 'ymax', 'xmax']):
+            return gemini_response
+
+    if isinstance(gemini_response, list) and len(gemini_response) > 0:
+        detection = gemini_response[0]
+        box = detection['box_2d']
+
+        return {
+            'ymin': box[0],
+            'xmin': box[1],
+            'ymax': box[2],
+            'xmax': box[3],
+            'label': detection.get('label', 'unknown')
+        }
+
+    raise ValueError(f"Unexpected response format: {type(gemini_response)} - {gemini_response}")
+
